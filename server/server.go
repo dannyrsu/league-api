5d47840be26f1e966635854197279eaa532b283d
Danny Su <dannyrsu@gmail.com>
2019-03-17 20:17:31 -0600

    Remove the key from flags

--- server/server.go
+++ server/server.go
@@ -20,11 +20,10 @@ import (
 )
 
 var (
-	tls        = flag.Bool("tls", false, "Coonection uses TLS if true")
-	certFile   = flag.String("cert_file", "", "The TLS cert file")
-	keyFile    = flag.String("key_file", "", "The TLS Key file")
-	port       = flag.Int("port", 50051, "The server port")
-	riotAPIKey = flag.String("riot_api_key", "", "Riot API Key")
+	tls      = flag.Bool("tls", false, "Coonection uses TLS if true")
+	certFile = flag.String("cert_file", "", "The TLS cert file")
+	keyFile  = flag.String("key_file", "", "The TLS Key file")
+	port     = flag.Int("port", 50051, "The server port")
 )
 
 type leagueServer struct{}
